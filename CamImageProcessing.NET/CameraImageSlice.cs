506202c32ed64b0700adc119c21efec2b73acbab
Andrej Lizunov <andrej.lizunov@outlook.com>
2016-07-19 17:07:38 +0600

    Fixes

--- CamImageProcessing.NET/CameraImageSlice.cs
+++ CamImageProcessing.NET/CameraImageSlice.cs
@@ -1,26 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-/// <summary>
-/// EmguCV namespaces
-/// </summary>
-using Emgu.CV;
-using Emgu.CV.Structure;
-using Emgu.CV.CvEnum;
-using Emgu.Util;
 
 namespace CamImageProcessing.NET
 {
     class CameraImageSlice
     {
-        // Uses source (large) Mat, but dont modify it
-        public Mat SrcMat
-        { get; }
-
     }
 }
