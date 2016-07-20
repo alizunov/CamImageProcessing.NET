ea01433e0b461b7a900c93bb2a0a865a5e47f138
Andrej Lizunov <andrej.lizunov@outlook.com>
2016-07-20 10:34:17 +0600

    Revert "fix"
    This reverts commit 989aaa2af621e6b39d6448002adf47c1138dcf28.

--- start_CamImageProcessing.NET.bat
+++
@@ -1,4 +0,0 @@
-@REM Use to start devenv
-@set ROOTSYS=C:\Users\lizunov\AppData\Local\Temp\root\root-5.34.36-vc12
-@set PATH=C:\Users\lizunov\AppData\Local\Temp\root\root-5.34.36-vc12\bin;%PATH%
-@F:\Users\lizunov\work\New projects\Plasma Imaging\Codes\CamImageProcessing.NET\CamImageProcessing.NET.sln
